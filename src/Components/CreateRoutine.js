d93b2b9f954c525a39ea8bb515d90febad87c0ec
Bryan Cardon <becard01@yahoo.com>
2022-12-28 22:12:11 -0700

    re-added missing apiCalls

--- src/Components/CreateRoutine.js
+++ src/Components/CreateRoutine.js
@@ -1,4 +1,21 @@
+import React from "react";
+import {createRoutine} from '../api/Requests.js'
+
+
 const CreateRoutine = (props) => {
+
+    const newRoutine = async () => {
+        try {
+              
+            const routine = await createRoutine(token, username);
+            console.log ("routines useEffect MyRoutines", routines)
+            setMyRoutines(routines);
+           }
+      
+        catch (error) {
+          console.error(error);
+            }
+      }
 
 
 }
