cd3a9c5cd857278e3abc6e90d569433dc05b1411
Bryan Cardon <becard01@yahoo.com>
2022-12-28 18:24:40 -0700

    added routes for create,edit, and delete routines in index.js

--- src/index.js
+++ src/index.js
@@ -29,6 +29,9 @@ const App = () => {
           <Route path = "/LoginRegister"><LoginRegister setToken = {setToken}></LoginRegister></Route>
           <Route path = "/Routines"> <Routines ></Routines></Route>
           <Route path = "/Activities"> <Activities></Activities></Route>
+          <Route path = "/MyRoutines/Create"> <CreateRoutine token = {token} ></CreateRoutine></Route>
+          <Route path = "/MyRoutines/Edit"> <EditRoutine token = {token} ></EditRoutine></Route>
+          <Route path = "/MyRoutines/Delete"> <DeleteRoutine token = {token} ></DeleteRoutine></Route>
           <Route path = "/MyRoutines"> <MyRoutines token = {token} ></MyRoutines></Route>
          
   </div>
