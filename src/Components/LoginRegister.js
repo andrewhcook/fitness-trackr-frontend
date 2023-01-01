f1ba35fb78758d1e65603fcbed6bf960fc58f47d
Nathan <nathan.wthamilton48@gmail.com>
2023-01-01 13:33:26 -0600

    feat: 'Thanks for signing up' added to register button

--- src/Components/LoginRegister.js
+++ src/Components/LoginRegister.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import { registerUser, logInUser } from '../api/Requests';
+import RegisterMessage from './RegisterMessage';
 
 const LoginRegister = (props) => {
     const setToken = props.setToken;
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [isShown, setIsShown] = useState(false);
+
     
     const [loginUsername, setLoginUsername] = useState("");
     const [loginPassword, setLoginPassword] = useState("");
@@ -32,6 +35,10 @@ const LoginRegister = (props) => {
          setLoginPassword("");
      }
 
+     const handleRegisterLogin = () => {
+        setIsShown(true);
+     }
+
     return (<><form className="login-form" onSubmit={onSubmitHandler}>
         <h1>SignUp Form</h1>
         <div className="field">
@@ -54,7 +61,14 @@ const LoginRegister = (props) => {
                 required
                 onChange={(event) => { setPassword(event.target.value); } } />
         </div>
-        <button className="submit-form" type="submit">Submit</button>
+        <button className="submit-form" type="submit" onClick={handleRegisterLogin}>Submit</button>
+        <div>
+            {isShown && <RegisterMessage />}
+        </div>
+
+        {/*register*/}
+
+
     </form>
         <form className="login-form" onSubmit={otherOnSubmitHandler}>
             <h1>Login Form</h1>
@@ -79,6 +93,9 @@ const LoginRegister = (props) => {
                     onChange={(event) => { setLoginPassword(event.target.value); } } />
             </div>
             <button className="submit-form" type="submit">Submit</button>
+            {/*login*/}
+
+
         </form></>);
     
     
---
+++ src/Components/RegisterMessage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RegisterMessage = (props) => {
+    return(
+        <h1>Thanks for signing up!</h1>
+    )
+}
+
+export default RegisterMessage
